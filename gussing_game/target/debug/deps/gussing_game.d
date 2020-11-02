01b4794181f15a3a34eca96b49fa04d4d6249089
masatoshi sugita <mr.f-r.ms621@i.softbank.jp>
2020-11-02 10:04:25 +0900

    2章の途中

---
+++ gussing_game/target/debug/deps/gussing_game.d
@@ -0,0 +1,5 @@
+/Users/sugitamasatoshi/rust01/gussing_game/target/debug/deps/gussing_game: src/main.rs
+
+/Users/sugitamasatoshi/rust01/gussing_game/target/debug/deps/gussing_game.d: src/main.rs
+
+src/main.rs:
---
+++ gussing_game/target/debug/gussing_game.d
@@ -0,0 +1,1 @@
+/Users/sugitamasatoshi/rust01/gussing_game/target/debug/gussing_game: /Users/sugitamasatoshi/rust01/gussing_game/src/main.rs
