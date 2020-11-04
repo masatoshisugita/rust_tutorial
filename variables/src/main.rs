f30531fd07ecd069f092cbdc1676396b3c5019fb
masatoshi sugita <mr.f-r.ms621@i.softbank.jp>
2020-11-04 12:03:46 +0900

    3-1まで

---
+++ variables/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    // let x = 5;
+    // println!("The values of x is: {}", x);
+    // x = 6;
+    // println!("The values of x is: {}", x);
+
+    // let mut x = 5;
+    // println!("The values of x is: {}", x);
+    // x = 6;
+    // println!("The values of x is: {}", x);
+
+    let x = 5;
+
+    let x = x + 1;
+
+    let x = x * 2;
+
+    println!("The values of x is: {}", x);
+
+}
